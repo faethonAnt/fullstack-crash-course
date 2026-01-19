cd73322e97a1e48af81b11a8f3e74f187b110f4f
Faethon Antonopoulos <antonopfaeth@gmail.com>
2026-01-19 23:40:40 +0200

    feat:used different types of strings to see the outcomes

---
+++ java-playground/sentenceMaker.js
@@ -0,0 +1,46 @@
+let adjective = "beautiful";
+let noun = "wife";
+let verb = "kissed";
+let place = "house";
+let adjective2 = "charming";
+let noun2 = "Litchis";
+const firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("First story: " + firstStory);
+adjective = "beautiful";
+noun = "husband";
+verb = "hug";
+place = "room";
+adjective2 = "lovely";
+noun2 = "oranges";
+const secondStory =
+  "As a happily married husband, I have a " +
+  adjective +
+  " " +
+  noun +
+  " that loves so much to eat " +
+  noun2 +
+  ". My " +
+  noun +
+  " Lives with me near " +
+  place +
+  " and she has a " +
+  adjective2 +
+  " smile and whenever i see her I like to " +
+  verb +
+  " her.";
+console.log("Second story: " + secondStory);
